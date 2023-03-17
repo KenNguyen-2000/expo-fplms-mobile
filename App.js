1285f50f6aa76529ea816cdd0a3f54710fd6a279
Nguyen Kien <ngkien299@gmail.com>
2023-03-17 16:43:24 +0700

    src

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/views/Login';
+import Signup from './src/views/Signup';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Login />
+      <Signup />
     </View>
   );
 }
@@ -16,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
+    fontWeight: 600,
+    textAlign: 'center',
   },
 });
---
+++ src/views/index.js
@@ -0,0 +1,3 @@
+import Login from './Login/Login.js.js.js.js';
+
+export { Login };
